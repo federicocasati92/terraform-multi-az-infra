c29350d4e00a2e71dfd46154380d3458353ce66d
federicocasati92 <federico.casati@outlook.com>
2025-08-20 21:54:29 +0200

    Add files via upload

---
+++ terraform-backend-setup/main.tf
@@ -0,0 +1,34 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "myproject-terraform-state-fc-34852"
+  acl    = "private"
+  
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
+  }
+}
+
